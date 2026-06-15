c05d2e10c568fefe1d87d6b96d66183a02291984
savadogo <savadogo.ab45@gmail.com>
2026-06-15 11:37:01 +0000

    creation de du primiere app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-
+import { BrowserRouter,Routers,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routers>
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routers>
+    </BrowserRouter>
   )
 }
